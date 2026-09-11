b6ae2532c5c5a339422b2077e9d4e12f198a8c64
Polyejik <kutuzovap@gmail.com>
2026-09-11 23:41:57 +0300

    Add Supabase client config

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.SAVEKITTY_SUPABASE = {
+  url: "https://axwgkbjnvjgfizzvxyfv.supabase.co",
+  anonKey: "sb_publishable_u7jgnOAg-JJaUpAa30R5YA_ZgbRBNv7",
+  campaignId: "save-pushok-pilot"
+};
