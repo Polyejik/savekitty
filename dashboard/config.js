73ed6f5dbed7e976e34b5ebb5989ddedfd727e43
Polyejik <kutuzovap@gmail.com>
2026-09-11 23:41:44 +0300

    Connect sponsor dashboard to Supabase

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,5 +1,5 @@
 window.SAVEKITTY_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://axwgkbjnvjgfizzvxyfv.supabase.co",
+  anonKey: "sb_publishable_u7jgnOAg-JJaUpAa30R5YA_ZgbRBNv7",
   campaignId: "save-pushok-pilot"
 };
